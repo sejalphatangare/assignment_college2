99106710e4bbc1c6901cfa86582cc493ad5ec662
sejalphatangare <sejal.phatangare@gmail.com>
2022-12-14 01:15:30 +0530

    added

--- House/src/House.java
+++ House/src/House.java
@@ -1,56 +1,48 @@
 import java.util.*;
-
-public class House {
-    private int vertices;
-    private int edges;
-    private int [] [] adjMat;
-    private Node[] head;
+public class House{
     Scanner sc=new Scanner(System.in);
-
-    public House(int vertices, int edges) {
-        this.vertices = vertices;
-        this.edges = edges;
+    int[][] adjMat =new int[10][10];
+    int vertices,edges,n;
+    public House() {
+        for(vertices=0;vertices<10;vertices++) {
+            for(edges=0;edges<10;edges++){
+                adjMat[vertices][edges]=0;
+            }
+        }
     }
+    public void create() {
+// assume undirected graph
+        System.out.println("\n\tHow many Houses You Want to Add: ");
+        n=sc.nextInt();
+        do {
+            System.out.println("\n\tEnter House Node: ");
+            vertices=sc.nextInt();
+            System.out.println("\n\tEnter Lane Node: ");
+            edges=sc.nextInt();
+            adjMat[vertices][edges]=1;
+            adjMat[edges][vertices]=1;
+            System.out.println("\n\tDo you want to add more House Number? Enter 1 for Yes And 0 for NO: ");
+        }while(sc.nextInt()==1);
 
-    public int getVertices() {
-        return vertices;
     }
-
-    public void setVertices(int vertices) {
-        this.vertices = vertices;
+    void show() {
+        System.out.println("\n\t*****Graph*****");
+        for(int i=0;i<n;i++) {
+            System.out.println("\n\t");
+            for(int j=0;j<n;j++) {
+                System.out.print("\t"+adjMat[i][j]);
+            }
+        }
     }
+    public void dfs() {
 
-    public int getEdges() {
-        return edges;
     }
+    public static void main(String args[]) {
+        House h1=new House();
+        h1.create();
+        h1.show();
 
-    public void setEdges(int edges) {
-        this.edges = edges;
-    }
-
-    public int[][] getAdjMat() {
-        return adjMat;
-    }
-
-    public void setAdjMat(int[][] adjMat) {
-        this.adjMat = adjMat;
-    }
-
-    public Node[] getHead() {
-        return head;
-    }
-
-    public void setHead(Node[] head) {
-        this.head = head;
-    }
-
-
-
-    public void createUsingAdjMat(){
-        System.out.println("\n\tEnter the Number of Houses: ");
-        setVertices(sc.nextInt());
-        System.out.println("\n\tEnter the Number of Lanes: ");
-        setEdges(sc.nextInt());
     }
 
 }
+
---
+++ House/src/ListGraph.java
@@ -0,0 +1,117 @@
+import java.util.*;
+public class ListGraph {
+    int v, w, n;
+    Scanner sc1=new Scanner(System.in);
+
+    List<Integer> a[];
+    Queue<Integer> q;
+    int desti;
+    public ListGraph(int n) {
+        a = new ArrayList[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = new ArrayList<>();
+        }
+        q=new LinkedList<>();
+    }
+    public void addEdge(int source,int desti){
+//        a[source].add(0,desti);
+//        a[desti].add(0,source);
+        a[source].add(desti);
+//        a[desti].add(source);
+
+    }
+
+    public void display() {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(i + "-->" + a[i]);
+        }
+    }
+    public void bfs(int start){
+        int visited[]=new int[20];
+        visited[start]=1;
+        q.add(start);
+        while(q.size()!=0){
+            int cur=q.poll();
+            System.out.print(cur+" ");
+            for (int i=0;i<a[cur].size();i++){
+                int ele=a[cur].get(i);
+                if (visited[ele]!=1){
+                    visited[ele]=1;
+                    q.add(ele);
+                }
+            }
+        }
+
+
+       /* graph.insertEdge(0, 1);
+        graph.insertEdge(0, 2);
+        graph.insertEdge(0, 3);
+        graph.insertEdge(1, 3);
+        graph.insertEdge(2, 4);
+        graph.insertEdge(3, 5);
+        graph.insertEdge(3, 6);
+        graph.insertEdge(4, 7);
+        graph.insertEdge(4, 5);
+        graph.insertEdge(5, 2);
+        graph.insertEdge(6, 5);
+        graph.insertEdge(7, 5);
+        graph.insertEdge(7, 8);
+        start=2
+        Answer = 2 4 7 5 8   */
+
+    }
+    public void dfs(int start){
+        int visited[]=new int[10];
+        Stack<Integer> s=new Stack<>();
+        visited[start]=1;
+        s.push(start);
+        System.out.print(start+" ");
+        while(!s.isEmpty()){
+            int cur=s.peek();
+            s.pop();
+            if (visited[cur]!=1){
+                System.out.print(cur+" ");
+                visited[cur]=1;
+            }
+            for (int i=0;i<a[cur].size();i++){
+                int ele=a[cur].get(i);
+                if (visited[ele]!=1){
+                    s.push(ele);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\n\tEnter the Number of Vertices: ");
+        int n=sc.nextInt();
+        ListGraph g=new ListGraph(n);
+        do{
+            System.out.println("\n\tEnter the Source: ");
+            int source = sc.nextInt();
+            System.out.println("\n\tEnter the Destination: ");
+            int desti = sc.nextInt();
+            g.addEdge(source, desti);
+            System.out.println("Do you want to continue adding node: ");
+        }while(sc.nextInt()==1);
+        g.display();
+       /* System.out.println("\n\tEnter the Source to Search if it is connected or not: ");
+        int temp_source=sc.nextInt();
+        System.out.println("\n\tEnter the destination to Search if it is connected or not: ");
+        int temp_desti=sc.nextInt();*/
+        System.out.println("\n\tEnter the Start Node: ");
+        int start=sc.nextInt();
+        g.bfs(start);
+        System.out.println("");
+        g.dfs(start);
+
+//        System.out.println(g.(temp_source,temp_desti));
+
+    }
+
+}
+
+
